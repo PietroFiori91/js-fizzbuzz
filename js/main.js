7a1054f9af9cdb6c4587b55a9db74e2dadc50062
PietroFiori91 <pederflowers@hotmail.it>
2023-07-05 11:01:30 +0200

    add comments

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
+// CICLO 1
+
 for (let i = 0; i <= 100; i++) {
   console.log(i);
 }
+
+//
 
 const cardBox = document.querySelector(".card-box");
 const cardSquare = 7 * 15;
 
 for (let i = 0; i < cardSquare; i++) {
-  // const singleSquare = '<div class="card-square"></div>';
-  // cardBox.innerHTML += singleSquare;
   let singleSquare = document.createElement("div");
   singleSquare.classList.add("card-square");
   singleSquare.innerHTML = i.toString();
+
+  // append & add class
 
   cardBox.append(singleSquare);
   singleSquare.classList.add(
@@ -21,15 +25,21 @@ for (let i = 0; i < cardSquare; i++) {
     "fw-bold"
   );
 
+  // MULTIPLI 3
+
   if (i % 3 === 0) {
     singleSquare.classList.add("bg-success");
     singleSquare.innerHTML = "Fizz";
   }
 
+  // MULTIPLI 5
+
   if (i % 5 === 0) {
     singleSquare.classList.add("bg-secondary");
     singleSquare.innerHTML = "Buzz";
   }
+
+  // MULTIPLI 3 & 5
 
   if (i % 3 === 0 && i % 5 === 0) {
     singleSquare.classList.add("bg-danger");
